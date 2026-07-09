805f6f55d45d16058f8a7185834120eaa16246ad
linuzenas <petluzenas@gmail.com>
2026-07-09 11:30:27 +0530

    swapping variables added

---
+++ PracticePython/Day 1/swapping variables.py
@@ -0,0 +1,3 @@
+a, b = 10, "shyam"
+a, b = b, a
+print(a,b)
--- PracticePython/Day 1/typecasting.py
+++ PracticePython/Day 1/typecasting.py
@@ -1,3 +1,7 @@
+import sys
+
 i = int(input('how old are you?: '))
 j = float(input('Evaluate 7/2: '))
 print('your age is', i, j)
+
+sys.stdout.write('hello')
