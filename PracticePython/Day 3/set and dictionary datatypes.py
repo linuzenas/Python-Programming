46874a6445e95243605f07e915b80cb9eb456e8b
linuzenas <petluzenas@gmail.com>
2026-07-13 14:43:30 +0530

    set and dictionary datatypes

---
+++ PracticePython/Day 3/set and dictionary datatypes.py
@@ -0,0 +1,14 @@
+'''
+set data type
+s1 = {"a", "a", "b", "c", "b"}
+print(s1)
+
+s2 = {"linu", "zenas", "paul"}
+for i in s2:
+    print(i)
+
+'''
+
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print(d[1])
+print(d.get(2))
