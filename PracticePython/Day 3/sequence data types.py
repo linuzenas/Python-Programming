3bea9afa042301ed00783c69abc57bb2dc2dd55c
linuzenas <petluzenas@gmail.com>
2026-07-13 14:37:03 +0530

    sequence data types

---
+++ PracticePython/Day 3/sequence data types.py
@@ -0,0 +1,24 @@
+'''
+string
+
+s = 'java python javascript'
+print(s)
+print(type(s))
+
+#acess with index
+print(s[1])
+print(s[-3])
+
+
+list
+a = [1, 2, 4, 6]
+print(a)
+
+#access with index
+print(a[-1])
+
+if we store multiple types in a single variable then it is tuple not list
+eg, a = ['python', 'java', 4, 5] tuple
+'''
+
+
